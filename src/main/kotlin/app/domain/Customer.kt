33699a630a8a5bf8951de52c43a60f898123e0d2
Tizor <peter.92@yandex.ru>
2019-08-02 10:59:00 +0300

    Do not show customers from DB fix it by adding annotations in domain

--- src/main/kotlin/app/domain/Customer.kt
+++ src/main/kotlin/app/domain/Customer.kt
@@ -1,24 +1,31 @@
 package app.domain
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.*
 import javax.persistence.*
 
 
 @Entity
 @Table(name = "customer")
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 data class Customer(
 
     @Id
+    @JsonProperty("id")
     @Column(name = "id")
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private val id: UUID? = null,
 
+    @JsonProperty("email")
     @Column(name = "email")
     private val email: String = "",
 
+    @JsonProperty("name")
     @Column(name = "name")
     private val name: String = "",
 
+    @JsonProperty("address")
     @Column(name = "address")
     private val address: String = ""
 
